442898555eeadaccaaeb1a022b585f9b6b9aa34d
Maynard Hartman <maynard.hartman@gmail.com>
2020-04-17 15:33:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
-class Pokemon
+class Pokemon < ActiveRecord::Base 
+  attr_accessor :name, :type, :id 
+  
+  def initialize(name, type) 
+    @name = name
+    @type = type
+    @id   = id
+  end
+  
+  def self.save(name, type)
+    sql = <<-SQL
+      #search if already entered 
+      SELECT * FEOM 'pokemon' WHERE name = ?
+      SQL
+      DB[:conn].execute(sql, name)
+      
+      sql == nil
+        sql = <<-SQL 
+          INSERT INTO 'pokemon' (id, name, type) VALUES (nil, ?, ?)
+        SQL
+      
+      DB[:conn].execute(sql, nil, self.name, self.type)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM 'pokemon'" )[0][0]
+      
+  end.first
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM 'pokemon' WHERE name = ?
+    SQL
+    
+    DB[:conn].execute(sql, name)
+    
+  end
 end
